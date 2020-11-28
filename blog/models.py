b27868aa63b9c80329eb96d285bcf17534b941bb
Adama <adamartukuli@gmail.com>
2020-11-28 15:19:41 -0600

    Final GoodFootPrint web finished

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,12 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=255)
-    pup_data = models.DateTimeField()
+    pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
